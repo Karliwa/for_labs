83881adcb7e0c6da5cdb8e6b1a588adc69872dea
Karliwa <lina.karlina.03@mail.ru>
2022-12-24 22:33:00 +0300

    6 laba

---
+++ data.sql
@@ -0,0 +1,16 @@
+/* 6 laba
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Макс', 'Максимович', 'Максимов', 'maksimov@my.example.com'),
+('Петр', 'Иванович', 'Ларионов', 'larka@my.example.com'),
+('Алекс', 'Мадагаскарович', 'Лещ', 'lew@my.example.com'),
+('Марко', 'Маркович', 'Макров', 'marka@ple.com'),
+('Рома', 'Романович', 'Романов', 'romabov@my.example.com'),
+('Петя', 'Иванович', 'Иванов', 'petuwa@my.example.com'),
+('Михаил', 'Иванович', 'Круг', 'zar@my.example.com'),
+('Евгения', 'Ивановна', 'Гречай', 'grechka@my.example.com'),
+('Рококо', 'Маркович', 'Иванов', 'rok@my.example.com'),
+('Матвей', 'Романов', 'Иванов', 'matka@my.example.com'),
+('Родион', 'Иванович', 'Петров', 'ivanovka@my.example.com');
+*/
