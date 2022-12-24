ff46354b164568843c577b9815f197931a6c8af5
Maestra228 <heymamidiknam@bk.ru>
2022-12-24 22:05:39 +0300

    5 laba

---
+++ create_db.sql
@@ -0,0 +1,7 @@
+/* 5 laba 
+CREATE USER ice_chocolate WITH PASSWORD 'admin'
+
+CREATE DATABASE karliwa WITH OWNER =ice_chocolate ENCODING = 'UTF8';
+
+GRANT ALL PRIVILEGES ON DATABASE karliwa to ice_chocolate;
+*/
